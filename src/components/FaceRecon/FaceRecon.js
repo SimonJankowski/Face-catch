b7e7f208a6f1acfdb57e3c13655fd76e001e9988
SimonJay <szyszkakontrast@gmail.com>
2021-11-22 21:44:26 +0100

    application FE routing added

--- src/components/FaceRecon/FaceRecon.js
+++ src/components/FaceRecon/FaceRecon.js
@@ -1,10 +1,14 @@
 import React from "react"
+import "./FaceRecon.css"
 
-const FaceRecon = ({ imageUrl }) => {
+const FaceRecon = ({ imageUrl, box }) => {
     return (
         <div className="center na">
             <div className="absolute mt2">
-                <img alt="image" src={imageUrl} width="500px" height="auto" />
+                <img id="imputimage" alt="" src={imageUrl} width="500px" height="auto" />
+                <div className="bounding-box"
+                    style={{ top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol }}
+                ></div>
             </div>
         </div>
     )
