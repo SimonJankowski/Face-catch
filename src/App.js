dddb3f2673dfa1734acc9a423340ef46ad4aafbc
SimonJay <szyszkakontrast@gmail.com>
2021-11-29 22:23:47 +0100

    adding new staff

--- src/App.js
+++ src/App.js
@@ -90,7 +90,7 @@ class App extends Component {
   }
   onPictureSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://nameless-ravine-84327.herokuapp.com', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
@@ -100,7 +100,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://nameless-ravine-84327.herokuapp.com', {
             method: 'put',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
